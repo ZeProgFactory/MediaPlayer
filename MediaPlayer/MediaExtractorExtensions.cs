cb8f5575afc7d8ff59177b7be21574d3f2fc2fac
Michael Engelmann <zeprogramfactory@outlook.com>
2019-05-03 05:48:12 +0200

    Code review ...

---
+++ MediaPlayer/MediaExtractorExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZPF.Media
+{
+   public static class MediaExtractorExtensions
+   {
+      public static async Task<IMediaItem> FetchMetaData(this IMediaItem mediaItem)
+      {
+         if (mediaItem.IsMetadataExtracted)
+            return mediaItem;
+
+         return mediaItem = await MediaPlayer.Current.MediaExtractor.CreateMediaItem(mediaItem);
+      }
+
+      public static async Task<IMediaItem[]> FetchMetaData(this IPlaylist playlist)
+      {
+         var mediaItems = playlist.Select(i => i.FetchMetaData());
+
+         return await Task.WhenAll(mediaItems);
+      }
+   }
+}
